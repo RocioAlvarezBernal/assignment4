880f736d60cbd167fc7113a893b0deda0925a00e
Andrew Sugg <sugg.andrew.lawrence@gmail.com>
2020-10-21 12:49:36 -0500

    practice

--- src/main/java/com/meritamerica/assignment4/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment4/MeritAmericaBankApp.java
@@ -3,5 +3,6 @@ package com.meritamerica.assignment3;
 public class MeritAmericaBankApp {
 	public static void main(String[] args) {
 		MeritBank.readFromFile("src/test/testMeritBank_bad.txt");
+	//edited
 	}
 }
